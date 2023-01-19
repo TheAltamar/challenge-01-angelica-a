d8c572782a2bbce102ca89393cbc95cbbb700d32
TheAltamar <an.altamar946@gmail.com>
2023-01-19 12:45:50 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function ageCheck(){
+    let age = prompt('What is your age?');
+    if (age <= 19){
+        alert("No buzz for you");
+    }else{
+        alert('You can get your beer');
+
+    }
+}
+
